b9a1310aa3c1c11de8f1e5b0a870781bc40ce41b
Robert-CPX <cpx@robbdev.xyz>
2023-11-10 13:12:27 +0800

    add icons

--- hardhat/hardhat.config.ts
+++ hardhat/hardhat.config.ts
@@ -3,6 +3,15 @@ import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.19",
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+    // polygon_mumbai: {
+    //   url: "hhttps://rpc-mumbai.maticvigil.com"",
+    //   accounts: [process.env.pk]
+    // },
+  },
 };
 
 export default config;
